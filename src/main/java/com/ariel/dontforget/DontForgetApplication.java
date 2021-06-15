8c7211088e00daf9f85ad60ac2419d68a470e684
ariel traful navarro <a.t.navarro.1993@gmail.com>
2021-06-15 01:02:03 -0300

    first commit

---
+++ src/main/java/com/ariel/dontforget/DontForgetApplication.java
@@ -0,0 +1,17 @@
+package com.ariel.dontforget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DontForgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontForgetApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/com/ariel/dontforget/DontForgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariel.dontforget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontForgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
